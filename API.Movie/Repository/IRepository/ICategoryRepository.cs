46b33e6dfa54fc4ec0a98ec26275da5cc1ade11c
alexasc24 <alexa.msc24@gmail.com>
2025-11-24 21:47:26 -0500

    new CategoryRepository

---
+++ API.Movie/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using API.Movie.DAL.Models;
+
+namespace API.Movie.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<ICollection<Category>> GetCategoriesAsync();
+        Task<Category> GetCategoryAsync(int id);
+
+        Task<bool> CategoryExistsByIdAsync(int id);
+
+        Task<bool> CategoryExistsByNameAsync(string name);
+
+        Task<bool> CreateCategoryAsync(Category catergory);
+
+        Task<bool> UpdateCategoryAsync(Category category);
+
+        Task<bool> DeleteCategoryAsync(int id);
+
+    }
+}
