9b267b29df3d201d28cbde8f05c69e471c35b4d6
alexasc24 <alexa.msc24@gmail.com>
2025-11-27 20:52:38 -0500

    new movieRepository created

---
+++ API.Movie/Repository/IRepository/IMovieRepository.cs
@@ -0,0 +1,17 @@
+﻿using API.Movie.DAL.Models;
+
+namespace API.Movie.Repository.IRepository
+{
+    public interface IMovieRepository
+    {
+        Task<ICollection<DAL.Models.Movie>> GetMoviesAsync();
+
+        Task<DAL.Models.Movie> GetMovieAsync(int id);
+
+        Task<bool> CreateMovieAsync(DAL.Models.Movie movie);
+
+        Task<bool> UpdateMovieAsync(DAL.Models.Movie movie);
+
+        Task<bool> DeleteMovieAsync(int id);
+    }
+}
---
+++ API.Movie/Repository/MovieRepository.cs
@@ -0,0 +1,59 @@
+﻿using API.Movie.DAL;
+using API.Movie.DAL.Models;
+using API.Movie.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Movie.Repository
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public MovieRepository(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+        public async Task<bool> CreateMovieAsync(DAL.Models.Movie movie)
+        {
+            movie.CreatedDate = DateTime.UtcNow;
+            await _context.Movies.AddAsync(movie);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> DeleteMovieAsync(int id)
+        {
+            var movie = await GetMovieAsync(id);
+            if (movie == null)
+            {
+                return false;
+            }
+            _context.Movies.Remove(movie);
+            return await SaveAsync();
+        }
+
+        public async Task<DAL.Models.Movie> GetMovieAsync(int id)
+        {
+            return await _context.Movies
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task<ICollection<DAL.Models.Movie>> GetMoviesAsync()
+        {
+            return await _context.Movies
+                .AsNoTracking()
+                .OrderBy(m => m.Name)
+                .ToListAsync();
+        }
+
+        public async Task<bool> UpdateMovieAsync(DAL.Models.Movie movie)
+        {
+            movie.ModifiedDate = DateTime.UtcNow;
+            _context.Movies.Update(movie);
+            return await SaveAsync();
+        }
+        private async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() >= 0 ? true : false;
+        }
+    }
+}
